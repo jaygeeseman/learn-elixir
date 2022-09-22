27eccb40f4af3e44a7dd1d46c141a64845123ba8
Jay Geeseman <jaygeeseman@users.noreply.github.com>
2022-09-22 07:27:13 +0000

    Finished to Formatting

--- animals/test/animals_test.exs
+++ animals/test/animals_test.exs
@@ -1,8 +1,14 @@
 defmodule AnimalsTest do
   use ExUnit.Case
-  doctest Animals
+  doctest Animals, except: [{:randomise, 1}, {:selection, 1}]
 
-  test "greets the world" do
-    assert Animals.hello() == :world
+  test "randomise" do
+    zoo = Animals.create_zoo
+    refute zoo == Animals.randomise(zoo)
+  end
+
+  test "selection" do
+    zoo = Animals.create_zoo
+    refute Animals.see_animals(zoo, 2) == Animals.selection(2)
   end
 end
